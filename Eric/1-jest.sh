8199898732369c28ea809c89183906a3ef4efd3e
bogstandard <eric.daddio@gmail.com>
2020-12-01 23:53:10 +0000

    WE CAN GO SMALLER

--- Eric/1-jest.sh
+++ Eric/1-jest.sh
@@ -1,4 +1,4 @@
 # expecting 262738554
 # tests 3 random values forever until it finds the correct bunch
-# 1 char shorter than the real answer
-IFS=$'\n' command eval 'z=(`<./i/1`)';f(){ r=${z[$RANDOM % ${#z[@]}]};};for((;;));{ f;a=$r;f;b=$r;f;c=$r;if((a+b+c==2020));then echo $[a*b*c];exit;fi;}
+# much shorter than the real answer
+IFS=$'\n' command eval 'z=(`<./i/1`)';f(){ r=${z[$RANDOM%${#z[@]}]};};for((;$[a+b+c]!=2020;));{ f;a=$r;f;b=$r;f;c=$r;};echo $[a*b*c]
