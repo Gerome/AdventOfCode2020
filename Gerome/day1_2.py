536da262960c5fb474395dc2fb63aba319b8526f
Gerome Marc Braddock <braddockgerome@gmail.com>
2020-12-01 18:26:53 +0000

    Hole in one

--- Gerome/day1_2.py
+++ Gerome/day1_2.py
@@ -2,9 +2,5 @@ from itertools import combinations
 
 from day1_1 import inputs
 
-sum_pairs = []
-for input1, input2, input3 in list(combinations(inputs, 3)):
-    if (input1 + input2 + input3) == 2020:
-        sum_pairs.append((input1, input2, input3))
-
+sum_pairs = [(input1, input2, input3) for input1, input2, input3 in list(combinations(inputs, 3)) if input1 + input2 + input3 == 2020]
 print(sum_pairs)
