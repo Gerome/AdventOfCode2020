df5a4cdac848c277484177eb96e0d63eb19d80c4
Gerome Marc Braddock <braddockgerome@gmail.com>
2020-12-02 16:52:24 +0000

    Part 2

--- Gerome/day2/day2_2.py
+++ Gerome/day2/day2_2.py
@@ -1,2 +1,18 @@
 
 
+with open("input.txt", "r") as f:
+    input = f.read().split('\n')
+
+count = 0
+for instance in input:
+    if not instance:
+        continue
+    index1 = int(instance.split('-')[0]) - 1
+    index2 = int(instance.split('-')[1].split()[0]) - 1
+    letter = instance.split('-')[1].split()[1][0]
+    password = instance.split('-')[1].split()[2]
+
+    if (password[index1] == letter and password[index2] != letter) or (password[index1] != letter and password[index2] == letter):
+        count += 1
+
+print(count)
