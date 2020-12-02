698fb42711b4f514c68c7d2f1f3662df1402c901
jabenjy <jabenjy@gmail.com>
2020-12-02 23:22:41 +0000

    reduce!

--- Ben/Day2/solution.js
+++ Ben/Day2/solution.js
@@ -1,23 +1,17 @@
 var fs = require('fs');
 var lines = fs.readFileSync(process.argv[2], 'utf8').split(/\r?\n/);
 
-function getParts(line) {
+let [p1,p2] = [0,0];
+lines.forEach(line => {
     let split = line.split(" ");
-    let c = split[1].replace(":","");
+    let c = split[1].replace(":", "");
     let [l, u] = split[0].split("-");
     let p = split[2];
-    return [c, l, u, p];
-}
-
-console.log(lines.filter(line => {
-    let [c, l, u, p] = getParts(line);
-    let o = (p.match(new RegExp(c,"g")) || []).length;
+    let o = (p.match(new RegExp(c, "g")) || []).length;
     if (o >= l && o <= u)
-            return true;
-}).length);
-
-console.log(lines.filter(line => {
-    let [c, l, u, p] = getParts(line);
+        p1++;
     if (p[l - 1] === c ^ p[u - 1] === c)
-        return true;
-}).length);
+        p2++;
+})
+
+console.log(`${p1}\n${p2}`)
