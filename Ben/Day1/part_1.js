c3fb5e7f3898d630b7a27374ccc8aba5a1a8c40c
jabenjy <jabenjy@gmail.com>
2020-12-02 22:32:39 +0000

    lower score

--- Ben/Day1/part_1.js
+++ Ben/Day1/part_1.js
@@ -1,11 +1,4 @@
 var fs = require('fs');
-
-// Make sure we got a filename on the command line.
-if (process.argv.length < 3) {
-    console.log('Usage: node ' + process.argv[1] + ' FILENAME');
-    process.exit(1);
-}
-
 var lines = fs.readFileSync(process.argv[2], 'utf8').split(/\r?\n/).map(Number);
 
 lines.forEach(i => {
