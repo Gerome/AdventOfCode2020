caf6d25c70a6e18d609a1575fe38475e8bb7d5ad
jabenjy <jabenjy@gmail.com>
2020-12-02 23:54:34 +0000

    I crave reductions

--- Ben/Day2/solution.js
+++ Ben/Day2/solution.js
@@ -3,14 +3,11 @@ var lines = fs.readFileSync(process.argv[2], 'utf8').split(/\r?\n/);
 
 let [p1,p2] = [0,0];
 lines.forEach(line => {
-    let [r,c,p] = line.split(" ");
-    c = c.replace(":", "");
-    let [l, u] = r.split("-");
+    let [l,u,c,p] = line.replace("-"," ").replace(":","").split(" ");
     let o = (p.match(new RegExp(c, "g")) || []).length;
     if (o >= l && o <= u)
         p1++;
     if (p[l - 1] === c ^ p[u - 1] === c)
         p2++;
 })
-
 console.log(`${p1}\n${p2}`)
