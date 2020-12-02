1e0890d6ef4cec17f71388c9d55f356175d5283d
jabenjy <jabenjy@gmail.com>
2020-12-02 23:42:58 +0000

    reduce day2

--- Ben/Day2/solution.js
+++ Ben/Day2/solution.js
@@ -3,10 +3,9 @@ var lines = fs.readFileSync(process.argv[2], 'utf8').split(/\r?\n/);
 
 let [p1,p2] = [0,0];
 lines.forEach(line => {
-    let split = line.split(" ");
-    let c = split[1].replace(":", "");
-    let [l, u] = split[0].split("-");
-    let p = split[2];
+    let [r,c,p] = line.split(" ");
+    c = c.replace(":", "");
+    let [l, u] = r.split("-");
     let o = (p.match(new RegExp(c, "g")) || []).length;
     if (o >= l && o <= u)
         p1++;
