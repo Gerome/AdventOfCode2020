792498aa89e6b831ad0d5b39cf172b3ca78499f6
Gerome Marc Braddock <braddockgerome@gmail.com>
2020-12-02 17:17:39 +0000

    Short as possible

--- Gerome/day2/day2_2.py
+++ Gerome/day2/day2_2.py
@@ -1,10 +1,10 @@
 
 
 with open("input.txt", "r") as f:
-    input = f.read().split('\n')
+    input = f.read()
 
 count = 0
-for instance in input:
+for instance in input.split('\n'):
     if not instance:
         continue
     index1 = int(instance.split('-')[0]) - 1
@@ -16,3 +16,18 @@ for instance in input:
         count += 1
 
 print(count)
+
+
+d=input
+
+c = 0
+for v in d.split('\n')[:-1]:
+    f=v.split('-')
+    i=int(f[0])-1
+    j=int(f[1].split()[0])-1
+    g=v.split()
+    l=g[1][0]
+    p=g[2]
+    if p[i]==l and p[j]!=l or p[i]!=l and p[j]==l:c+=1
+
+print(c)
