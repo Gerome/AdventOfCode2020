54d41e6b12e875db52a4fb09d189ddf8654cc1db
bogstandard <eric.daddio@gmail.com>
2020-12-02 21:21:16 +0000

    PRO SHRINKAGE

--- Eric/1-jest.sh
+++ Eric/1-jest.sh
@@ -1,4 +1,4 @@
 # expecting 262738554
 # tests 3 random values forever until it finds the correct bunch
 # much shorter than the real answer
-IFS=$'\n' command eval 'z=(`<./i/1`)';f(){ r=${z[$RANDOM%${#z[@]}]};};for((;$[a+b+c]!=2020;));{ f;a=$r;f;b=$r;f;c=$r;};echo $[a*b*c]
+IFS=$'\n' z=($(<i/1));f(){ r=${z[$RANDOM%${#z[@]}]};};for((;$[a+b+c]!=2020;));{ f;a=$r;f;b=$r;f;c=$r;};echo $[a*b*c]
