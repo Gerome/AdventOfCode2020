0b891aa0d5313a6a260f7789b53e2f82bd38494b
bogstandard <eric.daddio@gmail.com>
2020-12-02 19:03:42 +0000

    PRO BASH

---
+++ Eric/2.1.sh
@@ -0,0 +1,2 @@
+# expecting 467
+IFS=$'\n' command eval 'z=(`<i/2`)';for p in "${z[@]}";{ IFS=-;n=($p);IFS=' ';b=(${n[1]});l=${b[1]::1};c=${p//[^$l]};c=${#c}-1;for((;c>=n&&c<=b[0];i++)){ c=0; } };echo $i
