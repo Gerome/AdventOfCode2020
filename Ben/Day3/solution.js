2f9931e0383fe4200865c212511823766bef7d68
jabenjy <jabenjy@gmail.com>
2020-12-03 19:25:29 +0000

    tony hawks pro golfer

--- Ben/Day3/solution.js
+++ Ben/Day3/solution.js
@@ -1,6 +1,6 @@
-var i = require('fs').readFileSync('i', 'utf8').split(/\r?\n/);
-let rf = (starts) => (starts.map(s => {
-    for([xi, yi]=s,x=y=c=0;y+yi<i.length;i[y+=yi][x = (x + xi) % i[y].length] === '#'?c++:0);
-    return c;
-}).reduce((a, c) => a * c));
-console.log(rf([[3,1]]) + "\n" + rf([[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]))
+var i=require('fs').readFileSync('i','utf8').split(/\r?\n/);
+let r=(s)=>(s.map(s=>{
+    for([xi,yi]=s,x=y=c=0;y+yi<i.length;i[y+=yi][x=(x+xi)%i[y].length]==='#'?c++:0);
+    return c
+}).reduce((a,c)=>a*c));
+console.log(r([[3,1]])+"\n"+r([[1,1],[3,1],[5,1],[7,1],[1,2]]))
