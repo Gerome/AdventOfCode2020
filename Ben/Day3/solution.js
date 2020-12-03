fa957b97d16420b8839870aa0449ed1352785a7f
jabenjy <jabenjy@gmail.com>
2020-12-03 19:27:59 +0000

    change for clarity

--- Ben/Day3/solution.js
+++ Ben/Day3/solution.js
@@ -1,6 +1,6 @@
 var i=require('fs').readFileSync('i','utf8').split(/\r?\n/);
-let r=(s)=>(s.map(s=>{
-    for([xi,yi]=s,x=y=c=0;y+yi<i.length;i[y+=yi][x=(x+xi)%i[y].length]==='#'?c++:0);
+let r=(s)=>(s.map(n=>{
+    for([xi,yi]=n,x=y=c=0;y+yi<i.length;i[y+=yi][x=(x+xi)%i[y].length]==='#'?c++:0);
     return c
 }).reduce((a,c)=>a*c));
 console.log(r([[3,1]])+"\n"+r([[1,1],[3,1],[5,1],[7,1],[1,2]]))
