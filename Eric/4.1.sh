2a4a2664eb98be52c963930e7da34af5df884e1d
bogstandard <eric.daddio@gmail.com>
2020-12-04 18:35:17 +0000

    PRO BASH

---
+++ Eric/4.1.sh
@@ -0,0 +1,13 @@
+# expecting 254
+IFS=- z=($(awk '{print "-",$0}' RS= i/4))
+for p in "${z[@]}"
+{ 
+  IFS=' ' read -a y<<<${p//$'\n'/ }
+  c=0
+  for x in "${y[@]}"
+  {
+    [[ "byriyreyrhgthcleclpid" = *"${x::3}"* ]]&&((c++))
+  }
+  ((c>6))&&((t++))
+}
+echo $t
