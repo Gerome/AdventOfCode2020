7ab8d1202df4f53211bc52a3ad8d466daff5132e
Gerome Marc Braddock <braddockgerome@gmail.com>
2020-12-04 17:58:09 +0000

    day 4 p2 complete

--- Gerome/day4/day4_1.py
+++ Gerome/day4/day4_1.py
@@ -1,4 +1,16 @@
 
+required_values = ['byr','iyr','eyr','hgt','hcl','ecl','pid']
 
-for line in open("input.txt","r").readlines():
-    print(line)
+
+data =  open("input.txt","r").read()
+c = 0
+z = data.split('\n\n')
+for z in data.split('\n\n'):
+    x = z.replace('\n', ' ')
+    y = x.split()
+    fields = [i.split(':')[0] for i in y]
+    print(fields)
+    if len(list(set(fields) & set(required_values)))==7:
+        c+=1
+
+print(c)
