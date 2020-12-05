f2b79ad9833cb504b90d6084be78c1c46e48d6d9
jabenjy <jabenjy@gmail.com>
2020-12-05 23:32:05 +0000

    buzzed

--- Ben/Day5/solution.js
+++ Ben/Day5/solution.js
@@ -1,4 +1,4 @@
-i=require('fs').readFileSync('i','utf8').replace(/L|F/g,'0').replace(/R|B/g,'1').split(/\r?\n/)
+i=require('fs').readFileSync('i','utf8').replace(/L|F/g,0).replace(/R|B/g,1).split(/\r?\n/)
 n=(a=>("0b"+a)-0)
 i=i.map(p =>8*n(p.slice(0,7))+n(p.slice(7))).sort((a,b)=>a-b)
-console.log(i[i.length-1]+"\n"+(i.filter((a,b,c)=>a+1<c[b+1])[0]+1))
+console.log(i[i.length-1]+"\n"+(i.filter((a,b)=>a+1<i[b+1])[0]+1))
