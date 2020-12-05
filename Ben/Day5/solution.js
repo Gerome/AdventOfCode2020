5fd0b292f276e3cdccf2495224915d3fe49c32e5
jabenjy <jabenjy@gmail.com>
2020-12-05 23:05:33 +0000

    shave

--- Ben/Day5/solution.js
+++ Ben/Day5/solution.js
@@ -1,10 +1,12 @@
 i=require('fs').readFileSync('i','utf8').replace(/L|F/g,'0').replace(/R|B/g,'1').split(/\r?\n/)
-i=i.map(bp =>8*parseInt(bp.slice(0,7),2)+parseInt(bp.slice(7),2)).sort((a,b)=>a-b)
+n=(a=>("0b"+a)-0)
+i=i.map(p =>8*n(p.slice(0,7))+n(p.slice(7))).sort((a,b)=>a-b)
 c=console.log
 // Part 1
 c(i[i.length-1])
 // Part 2
 for(x=0;x<i.length;x++){
-    if (i[x]+1<i[x+1])
-        c(i[x]+1)
+    o=i[x]+1
+    if (o<i[x+1])
+        c(o)
 }
