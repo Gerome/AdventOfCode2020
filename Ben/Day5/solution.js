df185bd4140e6f6ebe98bec63a8d6e3bca119c08
jabenjy <jabenjy@gmail.com>
2020-12-06 15:49:02 +0000

    big savings!
    @bogstandard sorry but my bash keeps adding to your count for some reason

--- Ben/Day5/solution.js
+++ Ben/Day5/solution.js
@@ -1,4 +1,3 @@
 i=require('fs').readFileSync('i','utf8').replace(/L|F/g,0).replace(/R|B/g,1).split(/\r?\n/)
-n=(a=>("0b"+a)-0)
-i=i.map(p =>8*n(p.slice(0,7))+n(p.slice(7))).sort((a,b)=>a-b)
+i=i.map(p =>+("0b"+p)).sort((a,b)=>a-b)
 console.log(i.pop()+"\n"+(i.find((a,b)=>a+1<i[b+1])+1))
