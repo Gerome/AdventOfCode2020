b5adcc58baf31dd0717a4f2218e6dfa709f2ed50
Gerome Marc Braddock <braddockgerome@gmail.com>
2020-12-06 14:00:17 +0000

    fix

--- Gerome/day6/day6_2.py
+++ Gerome/day6/day6_2.py
@@ -2,7 +2,7 @@ data = open("input.txt","r").read()
 
 c = 0
 for line in data.split('\n\n'):
-    line_set = [set(x) for x in line.split('\n')]
+    line_set = [set(x) for x in line.split('\n') if x]
     common_chars = set.intersection(*line_set)
     c += len(common_chars)
 print(c)
