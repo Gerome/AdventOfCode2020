d9a07a029a3bf507701796fc9f74aa56b624e46b
Gerome Marc Braddock <braddockgerome@gmail.com>
2020-12-13 13:11:41 +0000

    fuck n.10 too

--- Gerome/day10/day10_2.py
+++ Gerome/day10/day10_2.py
@@ -1,4 +1,6 @@
 import math
+from itertools import groupby
+from operator import itemgetter
 
 input_lines = open("input.txt", "r").read()
 
@@ -7,25 +9,17 @@ split_lines = [int(i) for i in split_lines if i]
 split_lines = sorted(split_lines)
 
 
-def do_thing(current_number, remaining_values):
+def do_thing(remaining_values):
+    x = [list(map(itemgetter(1), g)) for k, g in groupby(enumerate(remaining_values), lambda x: x[0]-x[1])]
+    y = [len(i) for i in x if len(i) > 2]
+    z = []
+    for i in y:
+        if i == 5:
+            z.append(7)
+        else:
+            z.append(2**(i-2))
+    return z
 
-    numbers_within_limit = [value for value in remaining_values if current_number < value <= current_number + 3]
-
-    values_list = []
-
-    while numbers_within_limit[-1] != remaining_values[-1]:
-        list_length = len(numbers_within_limit)
-
-        if list_length > 1:
-            values_list.append(2**(list_length-2))
-
-        current_number = numbers_within_limit[-1]
-        remaining_values = remaining_values[remaining_values.index(current_number) + 1:]
-        numbers_within_limit = [value for value in remaining_values if current_number < value <= current_number + 3]
-
-    return values_list
-
-print(split_lines)
-result = do_thing(0, split_lines)
-print(result)
-print(math.prod(result))
+split_lines.insert(0, 0)
+split_lines.append(split_lines[-1]+3)
+print(math.prod(do_thing(split_lines)))
